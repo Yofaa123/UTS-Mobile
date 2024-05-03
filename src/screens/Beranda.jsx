6014c490835c10856bb395705124c3b32aa323a4
Yayukk <yayukoktafiana27@gmail.com>
2024-05-03 22:08:17 +0700

    Merubah tampilan pada Halaman utama

--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,14 +1,116 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IconMenu from '../component/IconMenu';
 
-const Beranda = () => {
+const Data = [
+  {id: 1, label: 'Sembako'},
+  {id: 2, label: 'Bumbu Dapur'},
+  {id: 3, label: 'Obat & Herbal'},
+  {id: 4, label: 'Alat Tulis'},
+  {id: 5, label: 'Snack-Snack'},
+];
+
+const SalesData = [
+  {id: 1, item: 'Gula', price: 'Rp 10.000'},
+  {id: 2, item: 'Garam', price: 'Rp 5.000'},
+  {id: 3, item: 'Merica', price: 'Rp 20.000'},
+  {id: 4, item: 'Buku', price: 'Rp 15.000'},
+  {id: 5, item: 'Kopi', price: 'Rp 25.000'},
+];
+
+const Beranda = ({navigation}) => {
   return (
-    <View>
-      <Text>Beranda</Text>
+    <View style={styles.container}>
+      <View style={styles.txt}>
+        <TouchableOpacity style={styles.ic}>
+          <Icon name="search" size={20} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.pencarian} />
+      </View>
+      <View style={styles.tbl}>
+        <Text style={styles.jdl}> Selamat Datang di Toko Kami</Text>
+      </View>
+      <Text style={styles.ktgr}>Kategori</Text>
+      <View>
+        <FlatList
+          data={Data}
+          renderItem={({item}) => <IconMenu label={item.label} />}
+          horizontal={true}
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Profil')}
+          style={styles.btn}>
+          <Text style={styles.ab}>Ke Profil</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Beranda;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  txt: {
+    //borderWidth: 1,
+    width: 360,
+    height: 70,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relaive',
+  },
+  pencarian: {
+    //borderWidth: 1,
+    width: 200,
+    height: 10,
+    margin: 20,
+    padding: 15,
+    borderRadius: 8,
+    width: 250,
+    marginLeft: -17,
+    backgroundColor: 'white',
+    position: 'relative',
+    top: 15,
+  },
+  tbl: {
+    margin: 10,
+    //borderWidth: 1,
+    height: 70,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+  },
+  jdl: {
+    fontSize: 20,
+    fontFamily: 'times',
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  ic: {
+    position: 'absolute',
+    left: 10,
+    top: '40%',
+    marginTop: 10,
+  },
+  ktgr: {
+    fontSize: 20,
+    color: 'black',
+    fontFamily: 'times',
+    fontWeight: 'bold',
+  },
+  btn: {
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: 'green',
+  },
+  ab: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
